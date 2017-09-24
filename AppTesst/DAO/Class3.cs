c26c9b9189a706500a271e326127df951f88bfb9
Hao <anhhao1996@gmail.com>
2017-09-24 20:39:52 +0700

    s

---
+++ AppTesst/DAO/Class3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTesst.DAO
+{
+    class Class3
+    {
+        public int testup()
+        {
+            return 1 + 3;
+        }
+    }
+}
