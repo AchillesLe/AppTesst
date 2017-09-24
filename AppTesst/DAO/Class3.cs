59bf3c6eec9a618dc9e68d9ef048f8bb8dac3acc
AchillesLe <AchillesLe@ACHILLESLE>
2017-09-24 20:46:34 +0700

    Change_3

--- AppTesst/DAO/Class3.cs
+++ AppTesst/DAO/Class3.cs
@@ -12,21 +12,10 @@ namespace AppTesst.DAO
         {
             return "Hào ăn .... heheheh !";
         }
-    }
-}
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AppTesst.DAO
-{
-    class Class3
-    {
         public int testup()
         {
             return 1 + 3;
         }
+        
     }
 }
